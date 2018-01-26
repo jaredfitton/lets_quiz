73db6d876d05a357670588f9e6aff78243451f63
jaredfitton <jaredfitton@gmail.com>
2018-01-26 09:22:12 -0800

    Added question_info and correct_questions

--- webapp.py
+++ webapp.py
@@ -24,41 +24,41 @@ def startOver():
 
 @app.route('/index',methods=['GET','POST'])
 def renderIndex():
-    #set the favorite color in the session
+
+    if "first_question" in request.form &&
     first_question = request.form["first_question"]
     second_question = request.form["second_question"]
     third_question = request.form["third_question"]
     fourth_question = request.form["fourth_question"]
-
     corect_questions = 0
+    question_info = {}
 
     if first_question == "-sinx":
-        session["first_question"] = "Correct"
+        question_info["first_question"] = "Correct"
         correct_questions += 1
     else:
-        session["first_question"] = "Incorrect"
+        question_info["first_question"] = "Incorrect"
 
     if second_question == "cosx":
-        session["second_question"] = "Correct"
+        question_info["second_question"] = "Correct"
         correct_questions += 1
     else:
-        session["second_question"] = "Incorrect"
+        question_info["second_question"] = "Incorrect"
 
     if third_question == "n(x^(n-1))":
-        session["third_question"] = "Correct"
+        question_info["third_question"] = "Correct"
         correct_questions += 1
     else:
-        session["first_question"] = "Incorrect"
+        question_info["first_question"] = "Incorrect"
 
     if fourth_question == "sec^2(x)":
-        session["fourth_question"] = "Correct"
+        question_info["fourth_question"] = "Correct"
         correct_questions += 1
     else:
-        session["fourth_question"] = "Incorrect"
+        question_info["fourth_question"] = "Incorrect"
 
-        
 
-    return render_template('index.html')
+    return render_template('index.html', response_question_info = question_info, response_correct_questions = correct_questions)
 
 if __name__=="__main__":
     app.run(debug=False)
