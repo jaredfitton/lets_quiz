50909b1461aaecbae4a30fb76c912fbcb016a7c3
jaredfitton <jaredfitton@gmail.com>
2018-01-26 09:38:13 -0800

    Added error message

--- webapp.py
+++ webapp.py
@@ -17,7 +17,7 @@ def renderMain():
     return render_template('index.html')
 
 @app.route('/startOver')
-def startOver():
+def clearScores():
     #clear variable values and create a new session
     session.clear()
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
@@ -25,40 +25,52 @@ def startOver():
 @app.route('/index',methods=['GET','POST'])
 def renderIndex():
 
-    if "first_question" in request.form &&
-    first_question = request.form["first_question"]
-    second_question = request.form["second_question"]
-    third_question = request.form["third_question"]
-    fourth_question = request.form["fourth_question"]
-    corect_questions = 0
-    question_info = {}
+    if all (i in request.form for i in ("first_question","second_question","third_question","fourth_question")):
+        first_question = request.form["first_question"]
+        second_question = request.form["second_question"]
+        third_question = request.form["third_question"]
+        fourth_question = request.form["fourth_question"]
+        corect_questions = 0
+        question_info = {}
 
-    if first_question == "-sinx":
-        question_info["first_question"] = "Correct"
-        correct_questions += 1
+        if first_question == "-sinx":
+            question_info["first_question"] = "Correct"
+            correct_questions += 1
+        else:
+            question_info["first_question"] = "Incorrect"
+
+        if second_question == "cosx":
+            question_info["second_question"] = "Correct"
+            correct_questions += 1
+        else:
+            question_info["second_question"] = "Incorrect"
+
+        if third_question == "n(x^(n-1))":
+            question_info["third_question"] = "Correct"
+            correct_questions += 1
+        else:
+            question_info["first_question"] = "Incorrect"
+
+        if fourth_question == "sec^2(x)":
+            question_info["fourth_question"] = "Correct"
+            correct_questions += 1
+        else:
+            question_info["fourth_question"] = "Incorrect"
+
+        score = correct_questions/4.0 * 100
+
+        session["question_info"] = question_info
+        session["score"] = score
+
+        if "highest_score" in session:
+            if session["highest_score"] < score:
+                session["highest_score"] = score
+        else:
+            session["highest_score"]
+
+        return render_template('index.html', response_question_info = question_info, response_score = score)
     else:
-        question_info["first_question"] = "Incorrect"
-
-    if second_question == "cosx":
-        question_info["second_question"] = "Correct"
-        correct_questions += 1
-    else:
-        question_info["second_question"] = "Incorrect"
-
-    if third_question == "n(x^(n-1))":
-        question_info["third_question"] = "Correct"
-        correct_questions += 1
-    else:
-        question_info["first_question"] = "Incorrect"
-
-    if fourth_question == "sec^2(x)":
-        question_info["fourth_question"] = "Correct"
-        correct_questions += 1
-    else:
-        question_info["fourth_question"] = "Incorrect"
-
-
-    return render_template('index.html', response_question_info = question_info, response_correct_questions = correct_questions)
+        return render_template('index.html', response_error_message = "Not All Questions Were Answered")
 
 if __name__=="__main__":
     app.run(debug=False)
